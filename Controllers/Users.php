f84659824b9f102d6dda571f9eeb757b515f56f6
Pablo Segura <srpablooficial@gmail.com>
2022-10-20 03:03:58 -0300

    changes

---
+++ Controllers/Users.php
@@ -0,0 +1,26 @@
+<?php
+
+class Users
+{
+
+    public function GET($param)
+    {
+        echo "GET_Users";
+    }
+
+    public function POST($param)
+    {
+        echo "POST_Users";
+    }
+
+    public function PUT($param)
+    {
+        echo "PUT_Users";
+    }
+
+    public function DELETE($param)
+    {
+        echo "DELETE_Users";
+    }
+
+}
---
+++ Controllers/Users_profile.php
@@ -0,0 +1,26 @@
+<?php
+
+class Users_profile
+{
+
+    public function GET($param)
+    {
+        echo "GET_Users_profile";
+    }
+
+    public function POST($param)
+    {
+        echo "POST_Users_profile";
+    }
+
+    public function PUT($param)
+    {
+        echo "PUT_Users_profile";
+    }
+
+    public function DELETE($param)
+    {
+        echo "DELETE_Users_profile";
+    }
+
+}
